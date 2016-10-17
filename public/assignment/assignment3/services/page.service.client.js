55b02bfd6e515cb1126f8cf159aec76bdc4d59d6
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2016-10-17 15:04:59 -0400

    problem in page-list.view.client.html

--- public/assignment/assignment3/services/page.service.client.js
+++ public/assignment/assignment3/services/page.service.client.js
@@ -9,8 +9,8 @@
     function PageService(){
         var pages = [
             { _id: 321, name: "Post 1", wid: 456},
-            { _id: "432", name: "Post 2", wid: "456" },
-            { _id: "543", name: "Post 3", wid: "456" }
+            { _id: "432", name: "Post 2", wid: 456 },
+            { _id: "543", name: "Post 3", wid: 456}
         ];
 
         var api =
