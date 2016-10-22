620e2ce612fa43c3389f969073e1ad7a6a8a76f9
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2016-10-22 17:43:59 -0400

    create widget logic has been modified, check why link is not working

--- public/assignment/assignment3/views/widget/widget-choose.controller.client.js
+++ public/assignment/assignment3/views/widget/widget-choose.controller.client.js
@@ -18,7 +18,7 @@
         vm.chooseWidget = chooseWidget;
         function chooseWidget()
         {
-            var widget = {};
+            /*var widget = {};
             var newWidget = WidgetService.createWidget(pageId, widget);
             if(newWidget)
             {
@@ -29,6 +29,61 @@
             else
             {
                 vm.error = "Unable to create widget";
+            }*/
+
+            vm.createHeaderWidget = createHeaderWidget;
+            function createHeaderWidget()
+            {
+                var widget = {"_id": 0, "widgetType": "HEADER", "pageId": pageId, "size": 0, "text": " "};
+                var newHeaderWidget = WidgetService.createWidget(pageId, widget);
+                vm.newHeaderWidget = newHeaderWidget;
+                if(newHeaderWidget)
+                {
+                    $location.url("/user/" + vm.userId + "/website/" +
+                        vm.websiteId + "/page/" + vm.pageId + "/widget/" + newHeaderWidget._id);
+                }
+            }
+
+
+            vm.createHtmlWidget = createHtmlWidget;
+            function createHtmlWidget()
+            {
+                var widget = {"_id": 0, "widgetType": "HTML", "pageId": pageId, "text": " "};
+                var newHeaderWidget = WidgetService.createWidget(pageId, widget);
+                vm.newHeaderWidget = newHeaderWidget;
+                if(newHeaderWidget)
+                {
+                    $location.url("/user/" + vm.userId + "/website/" +
+                        vm.websiteId + "/page/" + vm.pageId + "/widget/" + newHeaderWidget._id);
+                }
+            }
+
+
+            vm.createImageWidget = createImageWidget;
+            function createImageWidget()
+            {
+                var widget = {"_id": 0, "widgetType": "IMAGE", "pageId": pageId, "width": 0, "url": " "};
+                var newHeaderWidget = WidgetService.createWidget(pageId, widget);
+                vm.newHeaderWidget = newHeaderWidget;
+                if(newHeaderWidget)
+                {
+                    $location.url("/user/" + vm.userId + "/website/" +
+                        vm.websiteId + "/page/" + vm.pageId + "/widget/" + newHeaderWidget._id);
+                }
+            }
+
+
+            vm.createYoutubeWidget = createYoutubeWidget;
+            function createYoutubeWidget()
+            {
+                var widget = {"_id": 0, "widgetType": "YOUTUBE", "pageId": pageId, "width": 0, "url": " "};
+                var newHeaderWidget = WidgetService.createWidget(pageId, widget);
+                vm.newHeaderWidget = newHeaderWidget;
+                if(newHeaderWidget)
+                {
+                    $location.url("/user/" + vm.userId + "/website/" +
+                        vm.websiteId + "/page/" + vm.pageId + "/widget/" + newHeaderWidget._id);
+                }
             }
 
         }
