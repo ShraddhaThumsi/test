51e212f8951dd130234fc45cb62f22058da22609
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2016-12-05 10:50:45 -0500

    recipe search works

--- public/project/views/recipe/recipeSearch.controller.client.js
+++ public/project/views/recipe/recipeSearch.controller.client.js
@@ -6,7 +6,7 @@
         .module("RecipeMaker")
         .controller("RecipeSearchController", RecipeSearchController);
 
-    function RecipeSearchController($routeParams, $location) {
+    function RecipeSearchController($routeParams, $location, $http) {
         var vm = this;
         var userId =  $routeParams.uid;
         vm.userId = userId;
@@ -15,47 +15,56 @@
             console.log("You have asked for recipes on: " +  queryName);
             vm.queryName = queryName;
             var recipe = null;
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-
-                if (this.readyState == 4 && this.status == 200) {
-
-                    recipe = this.responseText;
-                    var jsonObject = JSON.parse(recipe);
-                    var hitArray = jsonObject.hits;
-                    console.log(hitArray);
-                    vm.hitArray = hitArray;
 
 
+            $http.get("https://api.edamam.com/search?app_id=be979c85&app_key=a6ded68b7dd66370c211045072bcb1a8&q=" + queryName)
+                .success(function (result) {
+                    console.log(result);
+                    vm.hitArray = result.hits;
+                });
 
-
-
-
-
-                   // document.getElementById("enterJSON").innerHTML = "";
-                    /*for (var hit in tempoArray) {
-                        var apiCallString = "http://api.edamam.com/search?app_id=be979c85&app_key=a6ded68b7dd66370c211045072bcb1a8&r=";
-                        var imageUri = tempoArray[hit].recipe.uri;
-                        var imageUriNew = imageUri.replace("#", "%23");
-                        document.getElementById("enterJSON").innerHTML +=
-                        " <a href ='" +
-                            apiCallString
-                            + imageUriNew + "' ><img src = '" + tempoArray[hit].recipe.image + "'>" +
-                            "</img></a>";
-
-                    }*/
-
-                }
-            };
-            var url = encodeURI("https://api.edamam.com/search?app_id=be979c85&app_key=a6ded68b7dd66370c211045072bcb1a8&q=" + queryName);
-
-
-            xhttp.open("GET", url, true);
-            xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
-            xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-            xhttp.withCredentials = false;
-
-            xhttp.send();
+            // var xhttp = new XMLHttpRequest();
+            // xhttp.onreadystatechange = function () {
+            //
+            //     if (this.readyState == 4 && this.status == 200) {
+            //
+            //         recipe = this.responseText;
+            //         var jsonObject = JSON.parse(recipe);
+            //         vm.hitArray = jsonObject.hits;
+            //
+            //         console.log(vm.hitArray);
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //        // document.getElementById("enterJSON").innerHTML = "";
+            //         /*for (var hit in tempoArray) {
+            //             var apiCallString = "http://api.edamam.com/search?app_id=be979c85&app_key=a6ded68b7dd66370c211045072bcb1a8&r=";
+            //             var imageUri = tempoArray[hit].recipe.uri;
+            //             var imageUriNew = imageUri.replace("#", "%23");
+            //             document.getElementById("enterJSON").innerHTML +=
+            //             " <a href ='" +
+            //                 apiCallString
+            //                 + imageUriNew + "' ><img src = '" + tempoArray[hit].recipe.image + "'>" +
+            //                 "</img></a>";
+            //
+            //         }*/
+            //
+            //     }
+            // };
+            // var url = encodeURI("https://api.edamam.com/search?app_id=be979c85&app_key=a6ded68b7dd66370c211045072bcb1a8&q=" + queryName);
+            //
+            //
+            // xhttp.open("GET", url, true);
+            // xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+            // xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+            // xhttp.withCredentials = false;
+            //
+            // xhttp.send();
 
         }
     }
